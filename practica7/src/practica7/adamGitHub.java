942c7afb3c95d09fd69a6b84497e89564849d315
Adam Aaloul Izariahen <adamakloul@gmail.com>
2023-12-13 20:00:03 +0100

    Primer commit - nuevo proyecto Java

---
+++ practica7/src/practica7/adamGitHub.java
@@ -0,0 +1,13 @@
+package practica7;
+public class adamGitHub {
+  public static void main(String[] args) {
+	System.out.println("Hola GitHub, sóc Adam Aaloul"); 
+	
+    imprimirNumeros();
+  }
+  public static void imprimirNumeros() {
+      for (int i = 1; i <= 20; i++) {
+          System.out.println(i);
+      }
+  }
+}
