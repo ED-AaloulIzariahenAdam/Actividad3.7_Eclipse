b4ccf15a8092f1ef22ba3f2de695ef99ab7a826f
Adam Aaloul Izariahen <adamakloul@gmail.com>
2023-12-13 20:04:59 +0100

    Segundo commit :Cambio en el .java

--- practica7/src/practica7/adamGitHub.java
+++ practica7/src/practica7/adamGitHub.java
@@ -2,6 +2,7 @@ package practica7;
 public class adamGitHub {
   public static void main(String[] args) {
 	System.out.println("Hola GitHub, sóc Adam Aaloul"); 
+	System.out.println("Cambio del GitHub");
 	
     imprimirNumeros();
   }
